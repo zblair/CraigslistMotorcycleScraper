6eae34ae07f8152b16367fbfa7c044c57eb97da5
Zachary Blair <125517+zblair@users.noreply.github.com>
2021-07-11 21:38:06 -0700

    Do not plot data points for years where the average price is zero

--- main.py
+++ main.py
@@ -2,55 +2,72 @@ from craigslist import CraigslistForSale
 from matplotlib import pyplot as plt
 import numpy
 
+
+class MotorcycleMake:
+    """A motorcycle make that will be plotted in the graph"""
+    def __init__(self, name, marker_format, line_format):
+        self.name = name
+        self.marker_format = marker_format
+        self.line_format = line_format
+        self.num_results = 0
+        self.sum_of_prices = 0
+
+
 # Parameters of the motorcycle search
 location = 'vancouver'
 years = range(1995, 2021)
-makes = ['Harley', 'Honda', 'Kawasaki', 'Triumph', 'Yamaha']
-marker_formats = {'Harley': 'yo', 'Honda': 'r.', 'Kawasaki': 'g+', 'Triumph': 'm.', 'Yamaha': 'co'}
-line_formats = {'Harley': 'y-', 'Honda': 'r--', 'Kawasaki': 'g:', 'Triumph': 'm-', 'Yamaha': 'c-.'}
+makes = [MotorcycleMake('Harley', 'yo', 'y--'),
+         MotorcycleMake('Honda', 'r.', 'r--'),
+         MotorcycleMake('Kawasaki', 'g+', 'g:'),
+         MotorcycleMake('Triumph', 'm.', 'm-'),
+         MotorcycleMake('Yamaha', 'co', 'c-.')]
 
 # Calculate the average price for each make and year
-average_prices = {}
-for make in makes:
-    average_prices[make] = []
+make_names = [m.name for m in makes]
+average_prices = {key: list() for key in make_names}
+for y in years:
+    print(f"Querying Craigslist for year {y}...")
+    cl = CraigslistForSale(site=location, category='mca', filters={'min_year': y, 'max_year': y})
+    results = cl.get_results()
 
-if __name__ == '__main__':
-
-    for y in years:
-        print(f"Querying Craigslist for year {y}...")
-        cl = CraigslistForSale(site=location, category='mca', filters={'min_year': y, 'max_year': y})
-        results = cl.get_results()
-
-        num_results = {}
-        sum_of_prices = {}
-        for make in makes:
-            sum_of_prices[make] = 0
-            num_results[make] = 0
-
-        # Get all of the results. Note that iterating over results actually makes requests to CL so we only
-        # want to iterate over results once and not put it in an inner loop
-        for result in results:
-            for make in makes:
-                if make in result['name']:
-                    num_results[make] = (num_results[make] + 1) if (make in num_results) else 1
-                    sum_of_prices[make] = (sum_of_prices[make] if (make in sum_of_prices) else 0) + int(result['price'].strip('$').replace(',', ''))
-
-        for make in makes:
-            average_price = (sum_of_prices[make] / num_results[make]) if num_results[make] > 0 else 0
-            average_prices[make].append(average_price)
-
-    # Plot the prices vs the year
-    print("Building an X-Y plot for average price vs model year for each make...")
     for make in makes:
-        plt.plot(years, average_prices[make], marker_formats[make], label=make)
+        make.sum_of_prices = 0
+        make.num_results = 0
 
-        # Show a trend line
-        z = numpy.polyfit(years, average_prices[make], 1)
-        p = numpy.poly1d(z)
-        plt.plot(years, p(years), line_formats[make], label=make)
+    # Get all of the results. Note that iterating over results actually makes requests to CL so we only
+    # want to iterate over results once and not put it in an inner loop
+    for result in results:
+        for make in makes:
+            if make.name in result['name']:
+                make.num_results = make.num_results + 1
+                make.sum_of_prices = make.sum_of_prices + int(result['price'].strip('$').replace(',', ''))
 
-    plt.title("Average Motorcycle Prices By Year and Make")
-    plt.xlabel('Year')
-    plt.ylabel('Price')
-    plt.figlegend()
-    plt.show()
+    for make in makes:
+        average_price = (make.sum_of_prices / make.num_results) if make.num_results > 0 else 0
+        average_prices[make.name].append(average_price)
+
+# Plot the prices vs the year
+print("Building an X-Y plot for average price vs model year for each make...")
+for make in makes:
+
+    # Ignore years for which we found no entries or the average price was zero
+    raw_prices = average_prices[make.name]
+    nonzero_prices = []
+    nonzero_years = []
+    for i in range(0, len(years)):
+        if raw_prices[i] > 0:
+            nonzero_years.append(years[i])
+            nonzero_prices.append(raw_prices[i])
+
+    plt.plot(nonzero_years, nonzero_prices, make.marker_format, label=make.name)
+
+    # Show a trend line
+    z = numpy.polyfit(nonzero_years, nonzero_prices, 1)
+    p = numpy.poly1d(z)
+    plt.plot(years, p(years), make.line_format, label=make.name)
+
+plt.title("Average Motorcycle Prices By Year and Make")
+plt.xlabel('Year')
+plt.ylabel('Price')
+plt.figlegend()
+plt.show()
